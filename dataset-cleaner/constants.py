8902b7dc28e2ef4cc8e858c8a1315d2b921e7dc6
aswinpradeep <aswinpradeep1@gmail.com>
2020-11-27 16:21:33 +0530

    API Changes

--- dataset-cleaner/constants.py
+++ dataset-cleaner/constants.py
@@ -1,6 +1,6 @@
-UPLOAD_URL   = "https://auth.anuvaad.org/api/v0/upload-file"
+UPLOAD_URL   = "https://auth.anuvaad.org/anuvaad-api/file-uploader/v0/upload-file"
 ALIGNER_URL  = "https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/async/initiate"
 DOWNLOAD_URL = "https://auth.anuvaad.org/download/"
 SEARCH_URL   = 'https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/jobs/search/bulk'
 
-HEADERS      = {'Authorization': 'Bearer XXXX_REPLACE_TOKEN_XXXX'}
+HEADERS      = {'auth-token': 'XXXX_REPLACE_TOKEN_XXXX'}
