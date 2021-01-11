fc6cad5f3353d8d7a7ac20c314e528ef2c03bc5d
Navneet3440 <67575306+Navneet3440@users.noreply.github.com>
2021-01-11 18:56:55 +0530

    Add files via upload

--- data-validation-pipeline/utilities/__init__.py
+++ data-validation-pipeline/utilities/__init__.py
@@ -2,4 +2,4 @@ from .app_context import MODULE_CONTEXT
 from .basic_cleaner.singularcleaner import singularcleanerfn
 from .basic_cleaner.parallelcleaner import parallelcleanerfn
 # from .hashing_encryption import hash_password,encrypt_password
-# from .userutils import generate_user_id
+from .number_sequence_corrector.number_sequence_corrector import number_sequence_corr
