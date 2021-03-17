33fee52f0e20e77fe249fbcd654edc41bfc025f2
PromodhPinto <69188809+PromodhPinto@users.noreply.github.com>
2021-03-17 11:52:54 +0530

    Add files via upload

---
+++ indianexpress-crawler/article_list.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import sys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+'''
+    argv[0] --> language,
+    argv[1] --> sections or topic.  '''
+
+lang = str(sys.argv[0])
+section = str(sys.argv[1])
+
+driver = webdriver.Chrome('chromedriver')
+
+if (lang == 'tamil'):
+    driver.get(f" 'https://tamil.indianexpress.com/section/{section}' ")
+elif (lang == 'hindi'):
+    driver.get(f" 'https://www.jansatta.com/section/{section}' ")
+elif (lang == 'english'):
+    driver.get(f" 'https://indianexpress.com/section/{section}' ")
+elif (lang == 'malayalam'):
+    driver.get(f" 'https://malayalam.indianexpress.com/section/{section}' ")
+elif (lang == 'bengali'):
+    driver.get(f" 'https://bengali.indianexpress.com/section/{section}' ")
+elif (lang == 'marathi'):
+    driver.get(f" 'https://www.loksatta.com/section/{section}' ")
+else:
+    print ("Please provide valid language") 
+    
+headings_list, dates_list, link_list = [],[],[] 
+count = 0
+
+def refresh():
+        try:
+            element = WebDriverWait(driver, 2).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[@class="next page-numbers"]')))
+            element.click()
+            time.sleep(1)
+        except Exception as e:
+            print(f"Not clickable...refreshing page {count}")
+            driver.back()
+            refresh()
+
+while True:
+    refresh()    
+    headings = driver.find_elements_by_xpath('//h2[@class="head"]')
+    for p in range(len(headings)):
+        headings_list.append(headings[p].text)
+        
+    date = driver.find_elements_by_xpath('//span[@class="byline"]')
+    for p in range(len(date)):
+        dates_list.append(date[p].text)
+    
+    elements = driver.find_elements_by_css_selector("h2.head a")
+    for element in elements:
+        link_list.append(element.get_attribute("href"))  
+    count=count+1
+
+df = pd.DataFrame({'Headings': headings_list, 'Date': dates_list, 'Link': link_list}) 
+time.sleep(1)
+print ("Complete")
+driver.quit()
+df.to_csv(f" '{lang}_articles_list_{section}_2021.csv' ",
+    mode='w',
+    encoding='utf-16',
+    index=False,
+)
+
---
+++ indianexpress-crawler/scrape_links.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+import requests
+import time
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
+
+'''
+    argv[0] --> output folder,
+    argv[1] --> month,
+    argv[2] --> language,
+    argv[3] --> section or topic
+    '''
+
+out_folder = str(sys.argv[0])
+month = str(sys.argv[1])
+language = str(sys.argv[2])
+section = str(sys.argv[4])
+
+september = pd.read_csv(f" '{out_folder}/{month}_{language}_articles_ie_{section}_2020.csv', encoding='utf-16' ")
+
+filenames = september['Headings']
+bad_chars = [';', ':', '!', '?', '|', '/', '"', '+', '<', '>', '.', "*"]
+count = 0
+
+start_time = time.time()
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+for link in september['Link'][0:]:
+    markup_string = requests.get(link, headers=headers, stream=True).content
+    soup = BeautifulSoup(markup_string, "html.parser")
+    content = soup.findAll(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'figcaption'])
+    filename = ''.join(i for i in filenames[count] if not i in bad_chars)
+    out_dir = f" '{out_folder}/{month}/{month}_{language}_articles_2020' "
+    
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    with open(os.path.join(out_dir, f"{count} {filename[:10]}.txt"), mode="w", encoding="utf-16") as file_w:
+        for text in range(len(content)):
+            file_w.write(content[text].text.strip() + "\r\n")       
+    count+=1
+    
+print(' %s seconds ' % (time.time() - start_time))
+
