3b1f02cb1058f76bd221dffa5d770d9cc364a3b2
PromodhPinto <69188809+PromodhPinto@users.noreply.github.com>
2021-03-17 12:14:48 +0530

    Delete config.py

--- indianexpress-crawler/config.py
+++
@@ -1,6 +0,0 @@
-CHROME_DRIVER_PATH = "chromedriver"
-DRIVER_WAIT_TIME = 20
-CSV_FILE_ENCODING = "utf-16"
-ANUVAAD_USERNAME = "navneet.kumar@tarento.com"
-ANUVAAD_PASSWORD = "Srikara@1234"
-BREAK_PROGRESS = True
