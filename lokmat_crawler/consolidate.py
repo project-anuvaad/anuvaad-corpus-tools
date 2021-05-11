a15c4074bcf25e4457488f8c692d8c8974bc19d6
PromodhPinto <69188809+PromodhPinto@users.noreply.github.com>
2021-05-11 12:33:16 +0530

    Add files via upload

---
+++ lokmat_crawler/consolidate.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd                
+
+import os
+import config
+lang = "Malayalam"
+lang_code = "ml"
+year= 2021
+source0, target0, source1, target1 = [], [], [], []
+month_list = [
+                "jan"
+            ]
+for month in month_list:
+    with open(f"{year}/Output/{year}/{month}_{year}/{lang}/align_{lang_code}_en/000_{lang_code}_sen_{month}_{year}_aligned_m_src.txt", encoding="utf-16") as sm:
+        for src0 in sm:
+            source0.append(src0.strip())
+    with open(f"{year}/Output/{year}/{month}_{year}/{lang}/align_{lang_code}_en/000_en_sen_{month}_{year}_aligned_m_tgt.txt", encoding="utf-16") as tm:
+        for tgt0 in tm:
+            target0.append(tgt0.strip())
+    with open(f"{year}/Output/{year}/{month}_{year}/{lang}/align_{lang_code}_en/000_{lang_code}_sen_{month}_{year}_aligned_am_src.txt", encoding="utf-16") as sam:
+        for src1 in sam:
+            source1.append(src1.strip())
+    with open(f"{year}/Output/{year}/{month}_{year}/{lang}/align_{lang_code}_en/000_en_sen_{month}_{year}_aligned_am_tgt.txt", encoding="utf-16") as tam:
+        for tgt1 in tam:
+            target1.append(tgt1.strip())
+
+match = pd.DataFrame({'English Sentences': target0, f" '{lang} Sentences' ": source0}) 
+almost_match = pd.DataFrame({'English Sentences': target1, f" '{lang} Sentences' ": source1})
+len_e = match.duplicated(subset=['English Sentences']).sum()
+len_b = match.duplicated(subset=[f" '{lang} Sentences' "]).sum()
+len_eb = match.duplicated(subset=['English Sentences']).sum()
+len_ae = almost_match.duplicated(subset=['English Sentences']).sum()
+len_ab = almost_match.duplicated(subset=[f" '{lang} Sentences' "]).sum()
+len_aeb = almost_match.duplicated(subset=['English Sentences']).sum()
+out_dir =f"Consolidated_Files/{year}"
+print(f"English {len_e}, {lang} {len_b}, En-{lang_code} Pair {len_eb}")
+print(f"English {len_ae}, {lang} {len_ab}, En-{lang_code} Pair {len_aeb}")
+print(len(match),len(almost_match))
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+match.drop_duplicates(subset=['English Sentences'], inplace=True)
+almost_match.drop_duplicates(subset=['English Sentences'], inplace=True)
+print(len(match),len(almost_match))
+match.to_csv(
+    os.path.join(out_dir, f"Total_IndianExpress_{lang_code}-En_{year}_Aligner_Match.csv"),
+    encoding=config.CSV_FILE_ENCODING,
+    index=False,
+)
+match.to_csv(
+    os.path.join(out_dir, f"Total_IndianExpress_{lang_code}-En_{year}_Aligner_Match.tsv"),
+    encoding=config.CSV_FILE_ENCODING,
+    index=False,
+    sep = '\t'
+)
+almost_match.to_csv(
+    os.path.join(out_dir, f"Total_IndianExpress_{lang_code}-En_{year}_Aligner_Almost_Match.csv"),
+    encoding=config.CSV_FILE_ENCODING,
+    index=False,
+)
+
---
+++ lokmat_crawler/run_sentence_tokenize.py
@@ -0,0 +1,206 @@
+import pandas as pd
+import os
+import glob
+from argparse import ArgumentParser
+from langdetect import detect
+from indicnlp.tokenize.sentence_tokenize import sentence_split
+from nltk.tokenize import sent_tokenize
+from pathlib import Path
+from langdetect import DetectorFactory
+
+DetectorFactory.seed = 0
+
+
+def create_directory(path):
+    try:
+        Path(path).mkdir(parents=True, exist_ok=True)
+        return True
+    except FileExistsError as fe_error:
+        return True
+    except OSError as error:
+        print(error)
+    return False
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--scrape-file-loc",
+        help="location of scrape file to be sentence splitted",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--output-folder", help="location of output folder", type=str, required=True
+    )
+    parser.add_argument("--month", help="month", type=str, required=True)
+    parser.add_argument("--year", help="year", type=str, required=True)
+    parser.add_argument(
+        "--lang",
+        help="language code :\
+                                        'Kannada':'kn',\
+                                        'Tamil':'ta',\
+                                        'Marathi':'mr',\
+                                        'Telugu':'te',\
+                                        'Bengali':'bn',\
+                                        'Gujarati':'gu',\
+                                        'Malayalam':'ml',\
+                                        'Punjabi':'pa',\
+                                        'Assamese':'asm',\
+                                        'Odia':'or',\
+                                        'Urdu':'ur'",
+        type=str,
+        required=True,
+    )
+    args = parser.parse_args()
+    n_month, n_year = args.month, args.year
+    print(n_month, n_year)
+    lang = args.lang
+    look_up_dict = {
+        "English": "en",
+        "Hindi": "hi",
+        "Kannada": "kn",
+        "Tamil": "ta",
+        "Marathi": "mr",
+        "Telugu": "te",
+        "Bengali": "bn",
+        "Gujarati": "gu",
+        "Malayalam": "ml",
+        "Punjabi": "pa",
+        "Assamese": "asm",
+        "Odia": "or",
+        "Urdu": "ur",
+    }
+    # Note: Inverting above dictionery
+    look_up_dict = {v: k for k, v in look_up_dict.items()}
+    if lang in look_up_dict.keys():
+        scrape_loc = args.scrape_file_loc
+        csv_file_loc = (
+            args.output_folder
+            + "//"
+            + n_month
+            + "_"
+            + n_year
+            + "//"
+            + look_up_dict[lang]
+        )
+        # list_fl = '_'.join([look_up_dict[lang],n_month,n_year]) + '.csv'
+        tokenize_loc = csv_file_loc + "//" + "tokenize_file_" + n_month + "_" + n_year
+        # submit_aligner = csv_file_loc + '\\' + 'submit_aligner'
+        if not os.path.exists(scrape_loc):
+            print(f"Path dosent exists:{scrape_loc}")
+            return
+        create_directory(csv_file_loc)
+        create_directory(tokenize_loc)
+
+    else:
+        print("Please enter the corrent langauge code")
+        return
+    total_sen_pd = pd.DataFrame(columns=[look_up_dict[lang] + "_sen"])
+    fl_list = sorted(glob.glob(os.path.join(scrape_loc, "*.txt")))
+    fl_list_rename = [
+        os.path.join(
+            scrape_loc,
+            "_".join(
+                [
+                    os.path.basename(i).split(".")[0].split()[0].zfill(5),
+                    *os.path.basename(i).split(".")[0].split()[1:6],
+                ]
+            ),
+        )
+        + "."
+        + os.path.basename(i).split(".")[-1]
+        for i in fl_list
+    ]
+    for org, chg in zip(fl_list, fl_list_rename):
+        os.rename(org, chg)
+    fl_list = sorted(glob.glob(os.path.join(scrape_loc, "*.txt")))
+    old_count = 0
+    for k, fl in enumerate(fl_list):
+        # print(os.path.basename(fl))
+        # Read Scrape Content
+        tok_flname = tokenize_loc + "//tok_" + os.path.basename(fl)
+        with open(fl, mode="r", encoding="utf-16") as file_r:
+            content = file_r.read()
+        #             print(content)
+        # Cleaning scrape content
+        paragraph = content.split("\n")
+        content = []
+        for para in paragraph:
+            para = para.strip()
+            para = " ".join(para.split())
+            if len(para.split()) >= 4:
+                if lang == "en":
+                    content.append(para)
+                else:
+                    try:
+                        if detect(para) != "en":
+                            content.append(para)
+                    except:
+                        content.append(para)
+        # Tokenizing paragraphs into sentences
+        sentences = []
+        if lang != 'en':
+            for entry in content:
+                [sentences.append(tok_sen) for tok_sen in sentence_split(entry, lang)]
+        else:
+            for entry in content:
+                    [sentences.append(tok_sen) for tok_sen in sent_tokenize(entry)]
+        # Removing Duplicates
+        dump_1 = (
+            pd.DataFrame(sentences, columns=["sen"])
+            .drop_duplicates()
+            .loc[:, "sen"]
+            .values.tolist()
+        )
+        sentences = dump_1
+        # Write sentence token
+        with open(tok_flname, mode="w", encoding="utf-16") as file_w:
+            for sen in sentences:
+                sen = sen.strip()
+                sen = sen.strip('"')
+                if len(sen.split()) >= 4:
+                    file_w.write(sen + "\n")
+                    total_sen_pd = total_sen_pd.append(
+                        {look_up_dict[lang] + "_sen": sen.strip()}, ignore_index=True
+                    )
+        # print(f'Number of sentences found: {total_sen_pd.shape[0]-old_count}')
+        old_count = total_sen_pd.shape[0]
+
+    print(f"Total number of sentences found: {total_sen_pd.shape[0]}")
+    total_sen_pd.drop_duplicates(inplace=True)
+    print(
+        f"Total number of sentences after removing duplicate: {total_sen_pd.shape[0]}"
+    )
+    total_sen_pd.to_csv(
+        csv_file_loc
+        + "//"
+        + "total_"
+        + lang
+        + "_sen_"
+        + n_month
+        + "_"
+        + n_year
+        + ".csv",
+        index=False,
+        encoding="utf-16",
+    )
+    with open(
+        csv_file_loc
+        + "//"
+        + "total_"
+        + lang
+        + "_sen_"
+        + n_month
+        + "_"
+        + n_year
+        + ".txt",
+        mode="w",
+        encoding="utf-16",
+    ) as write_total:
+        for line in total_sen_pd[look_up_dict[lang] + "_sen"].values.tolist():
+            write_total.write(line.strip() + "\n")
+
+
+if __name__ == "__main__":
+    main()
